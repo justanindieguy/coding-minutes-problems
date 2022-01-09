564aaab34c235e04213f4b6f86396541e36ff130
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2022-01-08 20:54:14 -0600

    implement basic tokenization with strtok function

---
+++ Strings/StringTokenizationStrtok/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char input[1000];
+    cin.getline(input, 1000);
+
+    // strtok()
+    // Internally maintains the state of the string you passed in the last fn call, static variable.
+    char *token = strtok(input, " ");
+    while (token != NULL)
+    {
+        cout << token << endl;
+        token = strtok(NULL, " ");
+    }
+
+    return 0;
+}
